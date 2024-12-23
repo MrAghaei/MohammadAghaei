8cc814f57f6b7e5e6388803b6c4f8b268d192b43
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-12-23 12:35:16 +0330

    [#2-ADD] hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header.tsx";
+import Hero from "./components/Hero.tsx";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
