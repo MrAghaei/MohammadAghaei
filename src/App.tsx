cf619a8ede4a03a25998449c79eb68ac9403d442
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-12-24 09:43:34 +0330

    [#3-ADD] about me

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header.tsx";
 import Hero from "./components/Hero.tsx";
+import AboutMe from "./components/AboutMe.tsx";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <AboutMe />
     </>
   );
 }
