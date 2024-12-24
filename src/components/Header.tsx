b7fb681197f58f2671bf64c2f49dc2c44a82aa22
Mohammad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-12-24 11:31:21 +0330

    [#6-ADD] projects section (#6)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="flex gap-40 items-center justify-center px-40 py-5 fixed mx-auto w-full bg-white bg-opacity-90">
+    <div className=" backdrop-blur-sm flex gap-40 items-center justify-center px-40 py-5 fixed mx-auto w-full bg-white bg-opacity-90">
       <p className="font-bold text-3xl">Mohammad Aghaei</p>
       <div className="flex items-center gap-10 text-2xl ">
         <button className="hover:bg-gray-200 rounded p-2 duration-300">
