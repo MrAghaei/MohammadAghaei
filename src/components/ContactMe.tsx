492f8d0a38801e04cd9737c3144bf5e69aee9b09
Mohammad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-12-24 12:33:11 +0330

    [#9-ADD] AOS (#9)

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -7,7 +7,11 @@ function Hero() {
     <div className="h-screen flex flex-col items-center justify-center gap-20 px-96 ">
       <h1 className="text-5xl font-bold">Contact Me</h1>
       <div className="flex gap-5">
-        <div className="flex flex-col shadow-xl hover:shadow-2xl duration-300 border border-blue-900 rounded-2xl p-5">
+        <div
+          data-aos-duration="1000"
+          data-aos="zoom-in"
+          className="flex flex-col shadow-xl hover:!shadow-2xl hover:!transition-shadow hover:!duration-300 border border-blue-900 rounded-2xl p-5"
+        >
           <div className="flex items-center gap-2">
             <FaTelegram className="size-6" />
             <a href="https://t.me/Mamad_Aghaei" target="_blank">
@@ -15,7 +19,11 @@ function Hero() {
             </a>
           </div>
         </div>
-        <div className="flex flex-col shadow-xl hover:shadow-2xl duration-300 border border-blue-900 rounded-2xl p-5">
+        <div
+          data-aos-duration="1000"
+          data-aos="zoom-in"
+          className="flex flex-col shadow-xl hover:!shadow-2xl hover:!transition-shadow hover:!duration-300 border border-blue-900 rounded-2xl p-5"
+        >
           <div className="flex items-center gap-2">
             <FaLinkedin className="size-6" />
             <a
@@ -26,7 +34,11 @@ function Hero() {
             </a>
           </div>
         </div>
-        <div className="flex flex-col shadow-xl hover:shadow-2xl duration-300 border border-blue-900 rounded-2xl p-5">
+        <div
+          data-aos-duration="1000"
+          data-aos="zoom-in"
+          className="flex flex-col shadow-xl hover:!shadow-2xl hover:!transition-shadow hover:!duration-300 border border-blue-900 rounded-2xl p-5"
+        >
           <div className="flex items-center gap-2">
             <MdEmail className="size-6" />
             <a href="mailto:mmdrezaaghaei@gmail.com" target="_blank">
--- src/components/Experiences.tsx
+++ src/components/Experiences.tsx
@@ -2,43 +2,51 @@ function Experiences() {
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-20 px-60 bg-gray-100">
       <h1 className="text-5xl font-bold">My Experiences</h1>
-      <div className="grid grid-cols-4 items-stretch gap-5  justify-center">
-        <div className="shadow-xl hover:shadow-2xl duration-300 flex flex-col items-start justify-center gap-2 border border-blue-900 rounded-2xl p-5">
-          <h3 className="text-2xl font-bold">
-            Content Manager & Graphic Designer
-          </h3>
-          <p>www.curlfans.com.au · Full-time</p>
-          <p className="opacity-80 text-sm">May 2021 - Jan 2022 · 9 months</p>
-        </div>
-        <div className="shadow-xl hover:shadow-2xl duration-300 flex flex-col items-start justify-center gap-2 border border-blue-900 rounded-2xl p-5">
-          <h3 className="text-2xl font-bold">Indie Game Developer</h3>
-          <p>52 Hertz Mind · Self-employed</p>
-          <p className="opacity-80 text-sm">
-            Jul 2019 - Jul 2023 · 4 years & 1 months
-          </p>
-        </div>
-        <div className="shadow-xl hover:shadow-2xl duration-300 flex flex-col items-start justify-center gap-2 border border-blue-900 rounded-2xl p-5">
-          <h3 className="text-2xl font-bold">Game Designer</h3>
-          <p>Nouranium · Full-time</p>
-          <p className="opacity-80 text-sm">
-            Jan 2022 - Mar 2023 · 1 year & 3 months
-          </p>
-        </div>
-        <div className="shadow-xl hover:shadow-2xl duration-300 flex flex-col items-start justify-center gap-2 border border-blue-900 rounded-2xl p-5">
-          <h3 className="text-2xl font-bold">Customer Service Administrator</h3>
-          <p>Dirclub.ir · Full-time</p>
-          <p className="opacity-80 text-sm">Oct 2023 - Jan 2024 · 4 months</p>
-        </div>
-        <div className="shadow-xl hover:shadow-2xl duration-300 flex flex-col items-start justify-center gap-2 border border-blue-900 rounded-2xl p-5">
-          <h3 className="text-2xl font-bold">Book Seller</h3>
-          <p>Cheshmeh Publication · Full-time</p>
-          <p className="opacity-80 text-sm">Apr 2024 - Oct 2024 · 7 months</p>
-        </div>
-        <div className="shadow-xl hover:shadow-2xl duration-300 flex flex-col items-start justify-center gap-2 border border-blue-900 rounded-2xl p-5">
-          <h3 className="text-2xl font-bold">Frontend Developer</h3>
-          <p>Bytepute · Full-time</p>
-          <p className="opacity-80 text-sm">Aug 2024 - Present · 5 months</p>
-        </div>
+      <div className="grid grid-cols-4 items-stretch gap-5 justify-center">
+        {[
+          {
+            title: "Content Manager & Graphic Designer",
+            company: "www.curlfans.com.au · Full-time",
+            duration: "May 2021 - Jan 2022 · 9 months",
+          },
+          {
+            title: "Indie Game Developer",
+            company: "52 Hertz Mind · Self-employed",
+            duration: "Jul 2019 - Jul 2023 · 4 years & 1 months",
+          },
+          {
+            title: "Game Designer",
+            company: "Nouranium · Full-time",
+            duration: "Jan 2022 - Mar 2023 · 1 year & 3 months",
+          },
+          {
+            title: "Customer Service Administrator",
+            company: "Dirclub.ir · Full-time",
+            duration: "Oct 2023 - Jan 2024 · 4 months",
+          },
+          {
+            title: "Book Seller",
+            company: "Cheshmeh Publication · Full-time",
+            duration: "Apr 2024 - Oct 2024 · 7 months",
+          },
+          {
+            title: "Frontend Developer",
+            company: "Bytepute · Full-time",
+            duration: "Aug 2024 - Present · 5 months",
+          },
+        ].map((experience, index) => (
+          <div
+            key={index}
+            data-aos="flip-down"
+            data-aos-easing="ease-in-back"
+            data-aos-duration="500"
+            className="shadow-xl hover:!shadow-2xl hover:!transition-shadow hover:!duration-300 flex flex-col items-start justify-center gap-2 border border-blue-900 rounded-2xl p-5"
+          >
+            <h3 className="text-2xl font-bold">{experience.title}</h3>
+            <p>{experience.company}</p>
+            <p className="opacity-80 text-sm">{experience.duration}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
