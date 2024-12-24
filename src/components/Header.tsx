c0f04d896805fc741376707013b05968e4b1d21e
Mohammad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-12-24 12:05:56 +0330

    [#8-ADD] buttons function (#8)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,43 @@
 function Header() {
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <div className=" backdrop-blur-sm flex gap-40 items-center justify-center px-40 py-5 fixed mx-auto w-full bg-white bg-opacity-90">
+    <div className="backdrop-blur-sm flex gap-40 items-center justify-center px-40 py-5 fixed mx-auto w-full bg-white bg-opacity-90">
       <p className="font-bold text-3xl">Mohammad Aghaei</p>
-      <div className="flex items-center gap-10 text-2xl ">
-        <button className="hover:bg-gray-200 rounded p-2 duration-300">
+      <div className="flex items-center gap-10 text-2xl">
+        <button
+          onClick={() => scrollToSection("about-me")}
+          className="hover:bg-gray-200 rounded p-2 duration-300"
+        >
           About Me
         </button>
-        <button className="hover:bg-gray-200 rounded p-2 duration-300">
+        <button
+          onClick={() => scrollToSection("experiences")}
+          className="hover:bg-gray-200 rounded p-2 duration-300"
+        >
           Experiences
         </button>
-        <button className="hover:bg-gray-200 rounded p-2 duration-300">
+        <button
+          onClick={() => scrollToSection("skills")}
+          className="hover:bg-gray-200 rounded p-2 duration-300"
+        >
           Skills
-        </button>{" "}
-        <button className="hover:bg-gray-200 rounded p-2 duration-300">
+        </button>
+        <button
+          onClick={() => scrollToSection("projects")}
+          className="hover:bg-gray-200 rounded p-2 duration-300"
+        >
           Projects
         </button>
-        <button className="hover:bg-gray-200 rounded p-2 duration-300">
+        <button
+          onClick={() => scrollToSection("contact-me")}
+          className="hover:bg-gray-200 rounded p-2 duration-300"
+        >
           Contact Me
         </button>
       </div>
